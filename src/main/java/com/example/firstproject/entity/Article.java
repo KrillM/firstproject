db9e67d3d939fe8634bf40a3852f7abcf4161968
krille <mks160615@gmail.com>
2025-08-27 19:46:43 +0900

    read

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -5,10 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @AllArgsConstructor // 생성자 자동 생성
+@NoArgsConstructor // 기본 생성자 추가 어노테이션
 @ToString // ToString 자동 상속
 public class Article {
 
