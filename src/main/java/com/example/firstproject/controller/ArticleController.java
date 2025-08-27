ab085a0f57ec092195ba9f2acd94f13fe984f116
krille <mks160615@gmail.com>
2025-08-27 19:14:53 +0900

    lombok

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j // 로깅 기능 제공
 public class ArticleController {
 
     @Autowired // 스프링부트에서 미리 생성한 Repository 객체 주입(DI: 의존성 주입)
@@ -22,13 +24,15 @@ public class ArticleController {
 
     @PostMapping("/article/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+//        System.out.println(form.toString());
 
         // 1. DTO를 엔터티로 변환
         Article article = form.toEntity();
+        log.info(article.toString());
 
         // 2. Repository로 엔터디를 DB에 저장
         Article saved = articleRepository.save(article);
+        log.info(saved.toString());
 
         return "";
     }
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor // 생성자 자동 생성
+@ToString // ToString 자동 상속
 public class Article {
 
     @Id
@@ -17,19 +21,4 @@ public class Article {
 
     @Column
     private String content;
-
-    public Article(Long id, String content, String title) {
-        this.id = id;
-        this.content = content;
-        this.title = title;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
